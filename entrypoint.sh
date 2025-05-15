33a2301ce75b9d8442f60c7535b39aa39c505060
Luan Estradioto <luanestradioto@gmail.com>
2025-05-14 23:29:07 -0300

    simplify

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f /src/db/cards.db ]; then
 	cp cards-empty.db /src/db/cards.db
+	cp cards-jwasham.db /src/db/cards-simple.db
+	cp cards-jwasham-extreme.db /src/db/cards-extreme.db
 fi
 
 export CARDS_SETTINGS=/src/config.txt
